6a735dbe52e5773d57a385201b508a14969c3225
Víctor Costa <victor.costa08@gmail.com>
2023-01-23 14:47:55 -0300

    added comments in entrypoint

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -11,6 +11,14 @@ then
     echo "PostgreSQL started"
 fi
 
+# si queremos que no se vacien los datos de la db y que se hagan las migraciones hay que descomentar las siguientes líneas
+# python manage.py flush --no-input
+# python manage.py migrate
+
+# manualmente se puede hacer así
+# $ docker-compose exec web python manage.py flush --no-input
+# $ docker-compose exec web python manage.py migrate
+
 python manage.py flush --no-input
 python manage.py migrate
 
